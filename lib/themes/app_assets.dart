702ab57e9d5fc939e01033b313876b519ebcf7ca
minhlongit <minhlongitf@gmail.com>
2024-09-16 09:37:35 +0700

    add app assets

---
+++ lib/themes/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  // images
+  static String get imgBrand => 'assets/images/brand.png';
+
+  // icons
+  static String get icApp => 'assets/icons/ic_flutter.png';
+
+  // splash screen
+  static String get imgSplash => 'assets/splash/logo.png';
+  static String get imgSplashAndroid12 => 'assets/splash/logo-android12.png';
+}
